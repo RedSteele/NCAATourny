53f67f931318e09690bf662efa9ccbe2fc79f6c6
Matthew Price <doggy1199@yahoo.com>
2013-03-15 09:07:41 -0600

    Created teamAt method

--- NCAARegion.java
+++ NCAARegion.java
@@ -9,11 +9,28 @@ class NCAARegion {
 	private String[] teams;
 	private int teamnum;
 	
+	/**
+	 * NCAARegion
+	 *
+	 * 		Constructor
+	 * 		
+	 * @param  teamnum - number of teams in the region
+	 * @param  teams - the array of teams in the region
+	 */
 	public NCAARegion(int teamnum, String[] teams) {
 		this.teamnum=teamnum;
 		this.teams = teams;
 	}
 
-
+	/**
+	 * teamAt
+	 *
+	 * 		returns the team at location n
+	 *
+	 * @param n - the team location
+	 */
+	public NCAATeam teamAt(int n) {
+		
+	}
 
 }
