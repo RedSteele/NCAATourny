06d2a5064deb558bac3761fb0d60fbd8a644a721
Matt <doggy1199@yahoo.com>
2013-03-28 13:36:04 -0600

    Added NCAAMain Class

---
+++ NCAAMain.java
@@ -0,0 +1,13 @@
+
+import java.io.FileNotFoundException;
+
+class NCAAMain {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		Bracket a = new Bracket();
+		a.playBracket();
+
+	}
+
+}
