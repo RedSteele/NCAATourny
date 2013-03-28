638391b19b40219bb4785025d6880c27128d6bda
anirudh <mathukumillia@s.dcsdk12.org>
2013-03-28 11:43:01 -0600

    team class finished

--- NCAATeam.java
+++ NCAATeam.java
@@ -1,6 +1,6 @@
 /**
  * NCAATeam
- * 		A class to create a team file including Win Percent, seed, and name
+ * 		Creates a team object
  */
 
 import java.util.Scanner;
@@ -8,8 +8,45 @@ import java.io.*;
 
 public class NCAATeam{
 	
-	public NCAATeam(){
+	private String seed;
+	private String name;
+	private String winPercent;
+	private String region;
+
+	/**
+	* 
+	* Constructor
+	*
+	* @param teamNumber - the number of the team you want to create(1-64) as designated by the file being read in from
+	**/
+	public NCAATeam(int teamNumber){
+		int teamNum = teamNumber - 1;
 		fileReader reader = new fileReader();
+		String[][] teams = reader.getTeams();
+		seed = teams[teamNum][1];
+		name = teams[teamNum][2];
+		winPercent = teams[teamNum][3];
+		region = teams[teamNum][4];
 	}
 
+	/**
+	* 
+	* getters
+	*
+	**/
+	public String getSeed(){
+		return seed;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getWinPercent(){
+		return winPercent;
+	}
+
+	public String getRegion(){
+		return region;
+	}
 }
